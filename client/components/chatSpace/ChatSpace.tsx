cebf00c8efda38965f0e5f8fa1e25aa6f211f2d2
unknown <negip7740@gmail.com>
2024-03-29 19:41:21 +0530

    design of chat space initialised

---
+++ client/components/chatSpace/ChatSpace.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './chatSpace.module.scss'
+
+const ChatSpace = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.chatInfo}>
+
+      </div>
+
+      <hr className={styles.infoDivider} />
+
+      <div className={styles.chatsContainer}>
+
+      </div>
+
+      <div className={styles.inputContainer}>
+
+      </div>
+    </div>
+  )
+}
+
+export default ChatSpace
